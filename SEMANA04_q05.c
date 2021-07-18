508144a7d1833f425c60b60d087fe0838d450c0e
Milena Franciele <86932918+milenafranciele@users.noreply.github.com>
2021-07-18 23:20:50 +0000

    EXE05 COMPLETO - SEMANA 04

---
+++ SEMANA04_q05.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void){
+  float a, b, c, maior, menores;
+
+  scanf("%f", &a);
+  scanf("%f", &b);
+  scanf("%f", &c);
+
+  if(a>b && a>c){
+    maior = a;
+    menores = b+c;
+  }
+
+  if(b>a && b>c){
+    maior = b;
+    menores = a+c;
+  }
+
+   if(c>a && c>b){
+    maior = c;
+    menores = a+b;
+  }
+
+
+  if(maior<menores){
+    if(a==b && a==c){
+      printf("EQUILATERO");
+    }else{
+      if(a!=b && a!=c && b!=c){
+        printf("ESCALENO");
+
+      }else{
+        printf("ISOSCELES");
+      }
+    }
+  
+  }else{
+    printf ("NAO FORMA");
+  }
+  
+}
