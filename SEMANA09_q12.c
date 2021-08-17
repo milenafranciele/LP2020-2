ffd828ab206d1f7c7960840d83a079aaef8c1e6e
Milena Franciele <86932918+milenafranciele@users.noreply.github.com>
2021-08-17 01:35:26 +0000

    SEMANA09 - EXE 14 COMPLETO

--- SEMANA09_q12.c
+++ SEMANA09_q12.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void){
-  int vetor[1000], i, j, tamVetor, exc = 0;
+// int main(void){
+//   int vetor[1000], i, j, tamVetor, exc = 0;
 
-  for(i=0; i<1000; i++){
-    scanf("%i", &vetor[i]);
-    if(vetor[i]<0){
-      tamVetor=i;
-      break;
-    }
-  }
+//   for(i=0; i<1000; i++){
+//     scanf("%i", &vetor[i]);
+//     if(vetor[i]<0){
+//       tamVetor=i;
+//       break;
+//     }
+//   }
   
-  for (j=0; j<tamVetor; j++){
-    if(vetor[j]>1000){
-      exc = 1;
-    }
-  }
+//   for (j=0; j<tamVetor; j++){
+//     if(vetor[j]>1000){
+//       exc = 1;
+//     }
+//   }
 
-  if(exc==1){
-    printf("DEU RUIM");
-  }else{
-    printf ("TURNO TRANQUILO");
-  }
-}
+//   if(exc==1){
+//     printf("DEU RUIM");
+//   }else{
+//     printf ("TURNO TRANQUILO");
+//   }
+// }
---
+++ SEMANA09_q14.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (void){
+  int vetor[1000], i, j, cont=0, tamVetor=0;
+
+  for(i=0; i<1000; i++){
+    scanf("%i", &vetor[i]);
+    if(vetor[i]<0){
+      tamVetor=i;
+      break;
+    }
+  }
+
+  for (i=0; i<=9; i++){
+    cont=0;
+    for (j=0; j<tamVetor; j++){
+      if(vetor[j]==i){
+        cont++;
+      }
+    }
+    if(cont>0){
+      printf("%i:%i\n",i,cont);
+    }
+  }
+  
+}
