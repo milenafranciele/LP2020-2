85a3d464656993632d6a054dfb0736ebbe5fef52
Milena Franciele <86932918+milenafranciele@users.noreply.github.com>
2021-08-17 00:19:04 +0000

    SEMANA09 -EXE 12 COMPLETO

--- SEMANA08_q11.c
+++ SEMANA08_q11.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void){
-  int i, num1, num2, maior;
+// int main(void){
+//   int i, num1, num2, maior;
 
-  scanf("%i", &num1);
-  scanf("%i", &num2);
+//   scanf("%i", &num1);
+//   scanf("%i", &num2);
 
-  if(num2>num1){
-    maior=num2;
-  }
-  else{
-    maior=num1;
-  }
+//   if(num2>num1){
+//     maior=num2;
+//   }
+//   else{
+//     maior=num1;
+//   }
 
-  for(i=maior; i>=1; i--){
-    if(num1%i==0 && num2%i==0){
-      printf("%i", i);
-      break;
-    }
-  }
-}
+//   for(i=maior; i>=1; i--){
+//     if(num1%i==0 && num2%i==0){
+//       printf("%i", i);
+//       break;
+//     }
+//   }
+// }
